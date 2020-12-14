afb1c13ed607a15a56956b3e86f1423f3ac1c2a6
Fede <chiellofederico@gmail.com>
2020-12-14 17:20:53 +0100

    variabili

--- js/script.js
+++ js/script.js
@@ -3,5 +3,12 @@
 // Viene chiesto all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
 // In base a questo, viene calcolato il prezzo totale del viaggio.
 
-var chilometri = parseInt(prompt('Inserire il numero dei km da percorrere'));
+var chilometri = parseFloat(prompt('Inserire il numero dei km da percorrere'));
+
 var eta = parseInt(prompt('Inserire l\'età del passeggero'));
+
+var prezzoBiglietto = parseFloat('0.21');
+
+console.log(prezzoBiglietto * chilometri);
+
+// document.getElementById('bigliettoTreno').innerHTML = prezzoBiglietto * chilometri;
