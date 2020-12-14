39fcf3fec5aa085eb3c9fed82090ea8e3d46ffc8
Fede <chiellofederico@gmail.com>
2020-12-14 16:32:40 +0100

    inserimento prompt

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-alert('Ciao a tutti');
+// Calcolo del prezzo del biglietto del treno
+
+// Viene chiesto all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// In base a questo, viene calcolato il prezzo totale del viaggio.
+
+var chilometri = parseInt(prompt('Inserire il numero dei km da percorrere'));
+var eta = parseInt(prompt('Inserire l\'età del passeggero'));
