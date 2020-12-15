1db337878d2c886f01741df9a0c688bdbdce14fb
Fede <chiellofederico@gmail.com>
2020-12-15 10:09:48 +0100

    Inserimento secondo metodo

--- js/script.js
+++ js/script.js
@@ -3,35 +3,37 @@
 // Viene chiesto all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
 // In base a questo, viene calcolato il prezzo totale del viaggio.
 
+// variabili
 var chilometri = parseFloat(prompt('Inserire il numero dei km da percorrere'));
-
 var eta = parseInt(prompt('Inserire l\'età del passeggero'));
-
-var prezzo = parseFloat('0.21');
-
+var prezzo = 0.21;
 var prezzoBiglietto = prezzo * chilometri;
-
-// console.log(prezzoBiglietto);
-
-// document.getElementById('bigliettoTreno').innerHTML = prezzoBiglietto;
-
-var eta;
-var prezzoBiglietto;
-var sconto = ((prezzoBiglietto * 20) / 100);
+var sconto = prezzoBiglietto * 20 / 100;
 var prezzoScontato = prezzoBiglietto - sconto;
-var prezzoFinale;
-var scontoOver = ((prezzoBiglietto * 40) / 100);
+var scontoOver = prezzoBiglietto * 40 / 100;
 var prezzoScontatoOver = prezzoBiglietto - scontoOver;
+var prezzoFinale;
 
+// primo metodo
+// if (eta < 18) {
+//   prezzoFinale = prezzoScontato;
+//   // console.log(prezzoScontato);
+// } else if (eta >= 65) {
+//   prezzoFinale = prezzoScontatoOver;
+//   // console.log(prezzoScontatoOver);
+// }
+// else {
+//   prezzoFinale = prezzoBiglietto;
+//   // console.log(prezzoBiglietto);
+// }
+
+// secondo metodo
 if (eta < 18) {
-  prezzoFinale = prezzoScontato;
+  prezzoBiglietto = prezzoScontato;
   // console.log(prezzoScontato);
 } else if (eta >= 65) {
-  prezzoFinale = prezzoScontatoOver;
+  prezzoBiglietto = prezzoScontatoOver;
   // console.log(prezzoScontatoOver);
-} else if(eta >= 18){
-  prezzoFinale = prezzoBiglietto;
-  // console.log(prezzoBiglietto);
 }
 
-document.getElementById('sconto').innerHTML = Math.trunc(prezzoFinale) + ' ' + '€';
+document.getElementById('bigliettoTreno').innerHTML = prezzoBiglietto.toFixed(2) + ' ' + '€';
